e633299ff920b562f7ce022356c33395ea91428d
Guerdy Lambert <guerdylambert9@gmail.com>
2026-01-09 15:36:49 -0500

    Fix Spring Boot test by excluding database auto-configuration

--- src/test/java/org/guerdy/lambert/ArieFrontendAutomationApplicationTests.java
+++ src/test/java/org/guerdy/lambert/ArieFrontendAutomationApplicationTests.java
@@ -2,12 +2,18 @@ package org.guerdy.lambert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration"
+})
 class ArieFrontendAutomationApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the Spring Boot application context loads successfully
+		// Database auto-configuration is excluded since this is a Selenium automation project
 	}
 
 }
